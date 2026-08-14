295fdd9b964da3506bdcbbf0ea4e14fee7e7d9aa
Senanurposul <posulsenanur@gmail.com>
2026-08-14 16:44:22 +0300

    feat: PackageModel update

--- src/utils/response.py
+++ src/utils/response.py
@@ -14,40 +14,47 @@ from components.SiftComparisonTest.src.models.PackageModel import (
 
 def build_response_sift_comparison_test(context):
 
+    # Detection output
     outputDetections = OutputDetections(
         value=context.output_detections
     )
 
+    # Visualization output
+    # Visualization henüz oluşturulmamışsa None döndürür.
     outputVisualization = OutputVisualization(
-        value=context.output_visualization
+        value=getattr(context, "output_visualization", None)
     )
 
+    # Outputs
     outputs = SiftComparisonTestOutputs(
         OutputDetections=outputDetections,
         OutputVisualization=outputVisualization
     )
 
+    # Response
     response = SiftComparisonTestResponse(
         outputs=outputs
     )
 
+    # Executor
     executor = SiftComparisonTest(
         value=response
     )
 
+    # Config Executor
     configExecutor = ConfigExecutor(
         value=executor
     )
 
+    # Package Config
     packageConfigs = PackageConfigs(
         executor=configExecutor
     )
 
+    # Package
     package = PackageHelper(
         packageModel=PackageModel,
         packageConfigs=packageConfigs
     )
 
-    packageModel = package.build_model(context)
-
-    return packageModel
+    return package.build_model(context)
